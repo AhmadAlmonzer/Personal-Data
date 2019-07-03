bb069f47491f115616f6301fc817fd367ae65411
Ahmad Almonzer <ahmad.alomonzer@gmail.com>
2019-07-03 02:44:34 +0200

    first commit

---
+++ Personal Data/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Personal_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.BtnSelectPhoto = new System.Windows.Forms.Button();
+            this.PicPerson = new System.Windows.Forms.PictureBox();
+            this.btnShowAllWithImages = new System.Windows.Forms.Button();
+            this.lblDT = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PicPerson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(274, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personal Data";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(147, 62);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(339, 20);
+            this.txtId.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.txtId, "Add your ID  please");
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(147, 117);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(339, 20);
+            this.txtName.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txtName, "Add your name please");
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(147, 175);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(339, 20);
+            this.txtAddress.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.txtAddress, "Add your address please");
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAdd.Location = new System.Drawing.Point(23, 230);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 43);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add New";
+            this.toolTip1.SetToolTip(this.btnAdd, "press to add your data");
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(92, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Address:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(387, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 43);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnShowAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowAll.Location = new System.Drawing.Point(266, 230);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(99, 43);
+            this.btnShowAll.TabIndex = 15;
+            this.btnShowAll.Text = "Show All";
+            this.toolTip1.SetToolTip(this.btnShowAll, "press to show all the data ");
+            this.btnShowAll.UseVisualStyleBackColor = false;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(147, 230);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(99, 43);
+            this.btnFind.TabIndex = 16;
+            this.btnFind.Text = "Find";
+            this.toolTip1.SetToolTip(this.btnFind, "press to find your data");
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // BtnSelectPhoto
+            // 
+            this.BtnSelectPhoto.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnSelectPhoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSelectPhoto.Location = new System.Drawing.Point(513, 279);
+            this.BtnSelectPhoto.Name = "BtnSelectPhoto";
+            this.BtnSelectPhoto.Size = new System.Drawing.Size(237, 43);
+            this.BtnSelectPhoto.TabIndex = 18;
+            this.BtnSelectPhoto.Text = "Select photo";
+            this.toolTip1.SetToolTip(this.BtnSelectPhoto, "press to add a pic");
+            this.BtnSelectPhoto.UseVisualStyleBackColor = false;
+            this.BtnSelectPhoto.Click += new System.EventHandler(this.BtnSelectPhoto_Click);
+            // 
+            // PicPerson
+            // 
+            this.PicPerson.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicPerson.Location = new System.Drawing.Point(513, 62);
+            this.PicPerson.Name = "PicPerson";
+            this.PicPerson.Size = new System.Drawing.Size(237, 211);
+            this.PicPerson.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicPerson.TabIndex = 17;
+            this.PicPerson.TabStop = false;
+            // 
+            // btnShowAllWithImages
+            // 
+            this.btnShowAllWithImages.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnShowAllWithImages.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowAllWithImages.Location = new System.Drawing.Point(23, 279);
+            this.btnShowAllWithImages.Name = "btnShowAllWithImages";
+            this.btnShowAllWithImages.Size = new System.Drawing.Size(463, 43);
+            this.btnShowAllWithImages.TabIndex = 19;
+            this.btnShowAllWithImages.Text = "Show All  With Images";
+            this.btnShowAllWithImages.UseVisualStyleBackColor = false;
+            this.btnShowAllWithImages.Click += new System.EventHandler(this.btnShowAllWithImages_Click);
+            // 
+            // lblDT
+            // 
+            this.lblDT.AutoSize = true;
+            this.lblDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDT.Location = new System.Drawing.Point(30, 23);
+            this.lblDT.Name = "lblDT";
+            this.lblDT.Size = new System.Drawing.Size(0, 17);
+            this.lblDT.TabIndex = 20;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.lblDT);
+            this.Controls.Add(this.btnShowAllWithImages);
+            this.Controls.Add(this.BtnSelectPhoto);
+            this.Controls.Add(this.PicPerson);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(800, 400);
+            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicPerson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox PicPerson;
+        private System.Windows.Forms.Button BtnSelectPhoto;
+        private System.Windows.Forms.Button btnShowAllWithImages;
+        private System.Windows.Forms.Label lblDT;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Personal Data/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Personal_Data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtId.Text.Trim() == "" || txtName.Text.Trim() == "" || txtAddress.Text.Trim() == "")
+                {
+                    MessageBox.Show("Please enter all the information");
+                    return;
+                }
+                 
+                StreamReader sr = new StreamReader("Data.txt");
+                string strCheck = sr.ReadToEnd();
+                sr.Close();
+
+                if (strCheck.Contains(txtId.Text + ";"))
+                {
+
+                    MessageBox.Show("this ID is alraedy used ; Please Enter another one");
+                    txtId.Focus();
+                    txtId.SelectAll();
+                }
+                else
+                {
+                    StreamWriter sw = new StreamWriter("Data.txt", true);
+                    string strPerson = txtId.Text + ";"
+                                     + txtName.Text + ";"
+                                     + txtAddress.Text;
+                    sw.WriteLine(strPerson);
+                    sw.Close();
+
+                    if (!Directory.Exists("img"))
+                    {
+                        Directory.CreateDirectory("img");
+                    }
+                    PicPerson.Image.Save("img/" + txtId.Text +".jpg");
+                    MessageBox.Show("Data added");
+                    foreach (Control c in this.Controls)
+                    {
+                        if (c is TextBox)
+                        {
+                            c.Text = "";
+                        }
+                        PicPerson.Image = new PictureBox().Image;
+                        txtId.Focus();
+
+                    }
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtId.Text.Trim() != "")
+                {
+                    StreamReader sr = new StreamReader("Data.txt");
+                    string line = "";
+                    bool Found = false;
+                    do
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            string[] arrData = line.Split(';');
+                            if (arrData[0] == txtId.Text)
+                            {
+                                txtId.Text = arrData[0];
+                                txtName.Text = arrData[1];
+                                txtAddress.Text = arrData[2];
+                                string pic = "img/" + arrData[0] + ".jpg";
+                                PicPerson.Image = Image.FromFile(pic);
+                                Found = true;
+                                break;
+                            }
+                        }
+                    } while (line != null);
+                    sr.Close();
+
+                    if (!Found)
+                    {
+                        MessageBox.Show("this ID is not found");
+                        foreach (Control c in this.Controls)
+                        {
+                            if (c is TextBox)
+                            {
+                                c.Text = "";
+                            }
+
+
+                        }
+                        txtId.Focus();
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("please Enter ID");
+                    txtId.Focus();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            Form frmDataShow = new Form();
+            TextBox txtDataShow = new TextBox();
+            frmDataShow.StartPosition = this.StartPosition;
+            frmDataShow.Size = this.Size;
+            frmDataShow.Text = "All Data";
+            txtDataShow.Multiline = true;
+            txtDataShow.Dock = DockStyle.Fill;
+            frmDataShow.Controls.Add(txtDataShow);
+            
+            try
+            {
+                StreamReader sr = new StreamReader("Data.txt");
+                string strDataShow = sr.ReadToEnd();
+                txtDataShow.Text = strDataShow;
+                sr.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            frmDataShow.ShowDialog();
+        }
+
+        private void BtnSelectPhoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Images|*.jpg;*.png;*.gif;*.bmp";
+            if (of.ShowDialog()==DialogResult.OK)
+            {
+                PicPerson.Image = Image.FromFile(of.FileName);
+            }
+        }
+
+        private void btnShowAllWithImages_Click(object sender, EventArgs e)
+        {
+            Form frmDataShow = new Form();
+            
+            frmDataShow.StartPosition = this.StartPosition;
+            frmDataShow.Size = this.Size;
+            frmDataShow.Text = "All Data With Images";
+            frmDataShow.Height += 300;
+            frmDataShow.AutoScroll = true;
+            int myTop = 10;
+
+            try
+            {
+                StreamReader sr = new StreamReader("Data.txt");
+                string line = "";
+                do
+                {
+                    line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        TextBox txt = new TextBox();
+                        PictureBox pic = new PictureBox();
+                        txt.Width = 300;
+                        txt.Top = myTop;
+                        txt.Text = line;
+                        pic.BorderStyle = BorderStyle.FixedSingle;
+                        pic.Left = 305;
+                        pic.Top = myTop;
+                        pic.Size = new Size(150, 150);
+                        pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                        string imgPath = "img/" + line.Split(';')[0] + ".jpg";
+                        if(File.Exists(imgPath))
+                        
+                            pic.Image = Image.FromFile(imgPath);
+                        frmDataShow.Controls.Add(txt);
+                        frmDataShow.Controls.Add(pic);
+                        
+                        myTop += 155;
+
+
+                    }
+                } while (line != null);
+
+                sr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            frmDataShow.ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDT.Text = DateTime.Now.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
